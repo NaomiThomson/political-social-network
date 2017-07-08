b4af81182b2693c8b058c9b30e2c6fa57bbe402b
Naomi Thomson <naomithomson94@gmail.com>
2017-07-07 17:49:19 -0700

    added get route for retrieving upcoming events

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -46,42 +46,55 @@ module.exports = function (app) {
          })
    });
 
+   // GET route for retrieving upcoming events
+   app.get('/api/events/date/:date', function (req, res) {
+      db.Event.findAll({
+            where: {
+               date: {
+                  gte: NOW()
+               }
+            }
+      })
+      .then(function (dbEvent) {
+         res.json(dbEvent)
+      })
+   });
+
    // POST route for saving a new event
    app.post('/api/events', function (req, res) {
       db.Event.create({
-         creator: req.body.creator,
-         location: req.body.location,
-         date: req.body.date,
-         title: req.body.title,
-         description: req.body.description
-      })
-      .then(function(dbEvent) {
-         res.json(dbEvent)
-      })
-   }); 
+            creator: req.body.creator,
+            location: req.body.location,
+            date: req.body.date,
+            title: req.body.title,
+            description: req.body.description
+         })
+         .then(function (dbEvent) {
+            res.json(dbEvent)
+         })
+   });
 
    // DELETE route for deleting events
-   app.delete('/api/events/:id', function(req, res) {
+   app.delete('/api/events/:id', function (req, res) {
       db.Event.destroy({
-         where: {
-            id: req.params.id
-         }
-      })
-      .then(function(dbEvent) {
-         res.json(dbEvent)
-      })
-   }); 
+            where: {
+               id: req.params.id
+            }
+         })
+         .then(function (dbEvent) {
+            res.json(dbEvent)
+         })
+   });
 
    // PUT route for updating events 
-   app.put('/api/events', function(req, res) {
-      db.Event.update(req.body, 
-      {
-         where: {
-            id: req.body.id
-         }
+   app.put('/api/events', function (req, res) {
+         db.Event.update(req.body, {
+            where: {
+               id: req.body.id
+            }
+         })
       })
-   })
-   .then(function(dbEvent) {
-      res.json(dbEvent)
-   });
+      .then(function (dbEvent) {
+         res.json(dbEvent)
+      });
 };
