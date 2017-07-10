d88900a968bb9a5aca3f378b82a5581b2dc207e5
Naomi Thomson <naomithomson94@gmail.com>
2017-07-09 20:18:15 -0700

    added validations to event model but datetime validation doesnt work

--- models/events.js
+++ models/events.js
@@ -1,11 +1,50 @@
-module.exports = function(sequelize, DataTypes) {
-   var Event = sequelize.define("Event", {
-      host: DataTypes.STRING,
-      title: DataTypes.STRING,
-      location: DataTypes.STRING, 
-      date: DataTypes.DATE,
-      description: DataTypes.TEXT
-   });
-   return Event;
-}
+module.exports = function (sequelize, DataTypes) {
 
+  function todaysDate() {
+    var today = moment().format('DD-MM-YYYY');
+    return today
+  }
+
+  function timeNow() {
+    var now = moment().format('hh:mm:ss');
+    return now
+  }
+
+
+  var Event = sequelize.define("Event", {
+    host: {
+      type: DataTypes.STRING
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    location: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        is: ["^[a-z]+$", 'i']
+      }
+    },
+    date: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+      validate: {
+        isAfter: todaysDate()
+      }
+    },
+    time: {
+      type: DataTypes.TIME,
+      allowNull: false, 
+      validate: {
+        isAfterTime(time) {
+          if (Date.parse(time) < Date.parse(timeNow())) {
+            throw new Error ('Event must be in the future!')
+          }
+        }
+      }
+    }, 
+    description: DataTypes.TEXT
+  });
+  return Event;
+}
