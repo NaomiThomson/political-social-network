63b56e1ca7ed482baf3a11ca10ff0d3a8cb499de
Maria <msawaby@gmail.com>
2017-07-15 08:58:54 -0700

    removing handlebars modification from local

--- models/index.js
+++ models/index.js
@@ -22,8 +22,8 @@ fs
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
   .forEach(function(file) {
-    var model = sequelize['import'](path.join(__dirname, file));
-    db[model.name] = model;
+    // var model = sequelize['import'](path.join(__dirname, file));
+    // db[model.name] = model;
   });
 
 Object.keys(db).forEach(function(modelName) {
